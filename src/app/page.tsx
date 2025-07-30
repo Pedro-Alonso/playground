59a205d1921447844ce2c69b61f867800112cf6a
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2025-07-30 12:02:44 -0300

    🎨 Improved Modal component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,31 @@
 "use client";
 
+import { Button, ButtonSizes, ButtonTypes } from "@/components/button/button";
 import { Modal } from "@/components/modal/modal";
 import { Screen } from "@/components/screen/screen";
+import { useTranslation } from "@/hooks/use-translation.hook";
 import { useState } from "react";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const translate = useTranslation("pages.home");
+
   return (
     <Screen className="gap-8">
-      <h1>Welcome to the React playground!</h1>
-      <button
-        className="bg-amber-900 w-40"
-        onClick={() => setIsOpen((prev) => !prev)}
-      >
-        Open modal
-      </button>
+      <h1>{translate("welcome")}</h1>
+      <Button
+        type={ButtonTypes.secondary}
+        size={ButtonSizes.medium}
+        onClick={() => setIsOpen(true)}
+        text="Abrir modal"
+      />
 
       <Modal
         isOpen={isOpen}
-        title="This is a modal"
-        closeOnClickOutside={false}
+        title="Isso é um modal"
+        closeOnClickOutside
+        onAction={() => setIsOpen(false)}
         onClose={() => setIsOpen(false)}
       />
     </Screen>
--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,19 +1,24 @@
+import { Button, ButtonSizes, ButtonTypes } from "@/components/button/button";
+import { useTranslation } from "@/hooks/use-translation.hook";
 import { createPortal } from "react-dom";
 
 interface ModalProps {
   title: string;
   isOpen: boolean;
-  onClose: () => void;
   closeOnClickOutside?: boolean;
+  onAction?: () => void;
+  onClose: () => void;
 }
 
 export const Modal = ({
   title,
   isOpen,
   closeOnClickOutside = true,
+  onAction,
   onClose,
 }: ModalProps) => {
   const target = document.getElementById("modal");
+  const translate = useTranslation("components.modal");
   return (
     isOpen &&
     target &&
@@ -25,13 +30,21 @@ export const Modal = ({
         />
         <div className="fixed inset-1/3 bg-blue-900 z-20">
           <div className="flex flex-col items-center justify-center w-full h-full gap-3">
-            <h1 className="text-yellow-400">{title}</h1>
-            <button
-              className="border-2 border-yellow-400 p-2"
+            <h1 className="text-yellow-400 mb-5">{title}</h1>
+            {onAction && (
+              <Button
+                type={ButtonTypes.primary}
+                size={ButtonSizes.medium}
+                text={translate("buttons.ok")}
+                onClick={onAction}
+              />
+            )}
+            <Button
+              type={ButtonTypes.secondary}
+              size={ButtonSizes.medium}
+              text={translate("buttons.close")}
               onClick={onClose}
-            >
-              Close modal
-            </button>
+            />
           </div>
         </div>
       </>,
