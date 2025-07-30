060b028d8d55821e8807dae65548023990a8b39b
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2025-07-29 22:24:03 -0300

    🔇 Removed logs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@
 
 import { Modal } from "@/components/modal/modal";
 import { Screen } from "@/components/screen/screen";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
-  useEffect(() => {
-    console.log("togglou papai, e dessa vez pra " + isOpen);
-  }, [isOpen]);
+
   return (
     <Screen className="gap-8">
       <h1>Welcome to the React playground!</h1>
